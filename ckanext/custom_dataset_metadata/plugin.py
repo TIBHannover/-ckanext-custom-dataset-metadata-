aaf04eb248ee918ebd0604f492a69a3b1456d917
pooya.oa <pooya.oladazimi@tib.eu>
2022-01-27 12:54:40 +0100

    test schema change

--- ckanext/custom_dataset_metadata/plugin.py
+++ ckanext/custom_dataset_metadata/plugin.py
@@ -2,8 +2,9 @@ import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
 
-class CustomDatasetMetadataPlugin(plugins.SingletonPlugin):
+class CustomDatasetMetadataPlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IDatasetForm)
 
     # IConfigurer
 
@@ -12,3 +13,40 @@ class CustomDatasetMetadataPlugin(plugins.SingletonPlugin):
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic',
             'custom_dataset_metadata')
+
+    
+
+    def _custom_dataset_metadata(self, schema):
+        schema.update({
+            'test_field': [toolkit.get_validator('ignore_missing'),
+                            toolkit.get_converter('convert_to_extras')]
+        })
+        return schema
+
+    def create_package_schema(self):
+        schema = super(CustomDatasetMetadataPlugin, self).create_package_schema()
+        schema = self._custom_dataset_metadata(schema)
+        return schema
+
+    def update_package_schema(self):
+        schema = super(CustomDatasetMetadataPlugin, self).update_package_schema()
+        schema = self._custom_dataset_metadata(schema)
+        return schema
+
+    def show_package_schema(self):
+        schema = super(CustomDatasetMetadataPlugin, self).show_package_schema()
+        schema.update({
+            'test_field': [toolkit.get_converter('convert_from_extras'),
+                            toolkit.get_validator('ignore_missing')]
+        })
+        return schema
+
+    def is_fallback(self):
+        # Return True to register this plugin as the default handler for
+        # package types not handled by any other IDatasetForm plugin.
+        return True
+
+    def package_types(self):
+        # This plugin doesn't handle any special package types, it just
+        # registers itself as the default (above).
+        return []
